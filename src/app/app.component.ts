8717e21012de4bc2aa4bf25f177b2237447ac26d
jvyden <jvyden@jvyden.xyz>
2023-03-06 22:23:31 -0500

    Add tailwind css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Refresh Website';
+  routerLinks: string[][] = [
+    ["Home", "/"],
+    ["Levels", "/levels"]
+  ]
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'display': ['Rubik'],
+      'body': ['Rubik']
+    }
+  },
+  plugins: [],
+}
