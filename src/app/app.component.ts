b1411ad2d7841f3adeff44e21e49ff95592ae44a
jvyden <jvyden@jvyden.xyz>
2023-03-06 19:17:35 -0500

    Drop "Prototype" from title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Refresh Website Prototype';
+  title = 'Refresh Website';
 }
