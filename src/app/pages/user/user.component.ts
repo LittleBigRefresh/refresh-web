3af24fad4f4b9dbc26c05d9d5d5e6f469282f469
jvyden <jvyden@jvyden.xyz>
2023-05-13 21:11:15 -0400

    Cleanup user component code

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,8 +1,8 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { ActivatedRoute, ParamMap, Route, Router } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import * as moment from 'moment';
-import { catchError, of } from 'rxjs';
+import { EMPTY, catchError, of, switchMap, tap } from 'rxjs';
 import { ApiClient } from 'src/app/api/api-client';
 import { Room } from 'src/app/api/types/rooms/room';
 import { User } from 'src/app/api/types/user';
@@ -18,63 +18,86 @@ export class UserComponent {
     constructor(private route: ActivatedRoute, private apiClient: ApiClient, private router: Router) {}
 
     ngOnInit(): void {
-      this.route.paramMap.subscribe((params: ParamMap) => {
+      this.route.paramMap.pipe(switchMap((params: ParamMap) => {
         const username = params.get('username') as string | undefined;
         const uuid = params.get('uuid') as string | undefined;
-        
-        if(username == "me" || uuid == "me") {
-          this.router.navigate(["/user/", this.apiClient.user?.Username]);
-          return;
-        }
-
-        if(uuid !== null && uuid !== undefined) {
-          this.apiClient.GetUserByUuid(uuid)
-          .pipe(catchError((error: HttpErrorResponse, caught) => {
-            console.warn(error)
-            if (error.status === 404) {
-              this.router.navigate(["/404"]);
-              return of(undefined)
-            }
     
-            return caught;
-          }))
-          .subscribe(data => {
-            this.user = data;
-            window.history.replaceState({}, '', `/user/${data?.Username}`);
-          });
-          return;
-        }
-
-        if(username == null) return;
-        
-        this.apiClient.GetUserByUsername(username)
-        .pipe(catchError((error: HttpErrorResponse, caught) => {
-          console.warn(error)
-          if (error.status === 404) {
-            this.router.navigate(["/404"]);
-            return of(undefined)
+        if (username == "me" || uuid == "me") {
+          if(this.apiClient.user === undefined) {
+            this.router.navigate(["/login"]);
+            return EMPTY;
           }
-  
-          return caught;
-        }))
-        .subscribe(data => {
-          this.user = data;
-          if(data === undefined) return;
 
-          this.apiClient.GetUsersRoom(data?.UserId)
-          .pipe(catchError((error: HttpErrorResponse, caught) => {
-            console.warn(error)
-            return of(undefined)
-          }))
-          .subscribe(data => {
-            this.room = data;
-          })
-        });
-      });
+          this.router.navigate(["/user/", this.apiClient.user?.Username]);
+          return EMPTY;
+        }
+    
+        if (uuid !== null && uuid !== undefined) {
+          return this.getUserByUuid(uuid);
+        }
+    
+        if (username == null) return EMPTY;
+    
+        return this.getUserByUsername(username);
+      }))
+      .subscribe();
     }
 
     getMoment(timestamp: number): string {
       if(timestamp == 0) return "Here since before time was invented";
       return "Joined " + moment(timestamp).fromNow();
+    }
+
+    private getUserByUuid(uuid: string) {
+      return this.apiClient.GetUserByUuid(uuid)
+        .pipe(
+          catchError((error: HttpErrorResponse, caught) => {
+            console.warn(error);
+            if (error.status === 404) {
+              this.router.navigate(["/404"]);
+              return of(undefined);
+            }
+    
+            return caught;
+          }),
+          tap((data) => {
+            this.user = data;
+            window.history.replaceState({}, '', `/user/${data?.Username}`);
+          })
+        );
+    }
+
+    private getUserByUsername(username: string) {
+      return this.apiClient.GetUserByUsername(username)
+        .pipe(
+          catchError((error: HttpErrorResponse, caught) => {
+            console.warn(error);
+            if (error.status === 404) {
+              this.router.navigate(["/404"]);
+              return of(undefined);
+            }
+    
+            return caught;
+          }),
+          tap((data) => {
+            this.user = data;
+            if (data === undefined) return;
+      
+            this.getUsersRoom(data?.UserId);
+          })
+        );
+    }
+    
+    private getUsersRoom(userId: string) {
+      return this.apiClient.GetUsersRoom(userId)
+        .pipe(
+          catchError((error: HttpErrorResponse, caught) => {
+            console.warn(error);
+            return of(undefined);
+          }),
+          tap((data) => {
+            this.room = data;
+          })
+        );
     }
 }
