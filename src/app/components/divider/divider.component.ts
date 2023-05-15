a982f7972afb2fb0bb2037f7120e0187f8ff5a28
jvyden <jvyden@jvyden.xyz>
2023-05-14 21:52:33 -0400

    Darken leaderboard to match style

--- src/app/components/divider/divider.component.ts
+++ src/app/components/divider/divider.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'divider',
   templateUrl: './divider.component.html'
 })
 export class DividerComponent {
+  _brightness: number = 700
 
+  @Input()
+  set brightness(brightness: number) {
+    this._brightness = brightness;
+  }
 }
