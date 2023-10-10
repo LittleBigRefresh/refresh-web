a4d211a90d713a7a375204d9585070fe03e8d765
jvyden <jvyden@jvyden.xyz>
2023-10-09 20:19:59 -0400

    Add some nice shadowing to buttons and things

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,4 +3,5 @@ import { IEnvironment } from "./environment-interface";
 export const environment: IEnvironment = {
     production: false,
     apiBaseUrl: "http://127.0.0.1:10061/api/v3"
+    // apiBaseUrl: "https://refresh.jvyden.xyz/api/v3"
 };
