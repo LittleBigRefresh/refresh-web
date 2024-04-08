4fba76c48fc7c37b2574c52d5ff03f7c3b6bec28
jvyden <jvyden@jvyden.xyz>
2024-04-07 22:29:08 -0400

    Minor improvements

--- src/app/api/types/level-edit-request.ts
+++
@@ -1,8 +0,0 @@
-import {GameVersion} from "./game-version";
-
-export interface LevelEditRequest {
-    title: string | undefined;
-    description: string | undefined;
-    iconHash: string | undefined;
-    gameVersion: string | undefined;
-}
--- src/environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-import { IEnvironment } from "./environment-interface";
-
-export const environment: IEnvironment = {
-    production: false,
-    apiBaseUrl: "http://127.0.0.1:10061/api/v3"
-    // apiBaseUrl: "https://refresh.jvyden.xyz/api/v3"
-};
