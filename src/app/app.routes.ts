dc8ea51ea9306616522fa716d3582291127bc356
jvyden <jvyden@jvyden.xyz>
2024-04-10 00:39:57 -0400

    Lazy chunks

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
-import {LandingComponent} from "./pages/landing/landing.component";
 
 export const routes: Routes = [
     {
         path: '',
-        component: LandingComponent
+        loadComponent: () => import('./pages/landing/landing.component').then(x => x.LandingComponent)
     }
 ];
