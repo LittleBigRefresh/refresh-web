3bb10c36e7676c627f51c044d79e99dea93c9380
Noble <noblereign@users.noreply.github.com>
2024-05-07 01:34:55 -0700

    i should move that below actually
    for consistency
    
    Signed-off-by: Noble <noblereign@users.noreply.github.com>

--- src/app/services/embed.service.ts
+++ src/app/services/embed.service.ts
@@ -47,8 +47,8 @@ export class EmbedService {
 
     embedInstance(instance: Instance) {
         if (this.meta.getTag('name="og:title"')) return;
-        this.setPropertyTag("og:site_name", instance.instanceName)
         this.embed(`${instance.instanceName} - ${instance.instanceDescription}`, `${instance.instanceName} is a free custom server for LittleBigPlanet.`);
+        this.setPropertyTag("og:site_name", instance.instanceName)
         this.setPropertyTag("og:type", "website");
     }
 
