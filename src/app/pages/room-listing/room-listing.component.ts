c1565c71d546c794a9d6ad32bc8ac40eab8d2eef
jvyden <jvyden@jvyden.xyz>
2024-05-22 21:23:31 -0400

    better-ish design. this looks shit

--- src/app/pages/room-listing/room-listing.component.ts
+++ src/app/pages/room-listing/room-listing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {PageTitleComponent} from "../../components/ui/text/page-title.component";
 import {RoomComponent} from "../../components/items/room.component";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {Room} from "../../api/types/rooms/room";
 import {ClientService} from "../../api/client.service";
 import {GameVersion} from "../../api/types/game-version";
@@ -9,6 +9,7 @@ import {ContainerComponent} from "../../components/ui/container.component";
 import {ContainerTitleComponent} from "../../components/ui/text/container-title.component";
 import {GamePipe} from "../../pipes/game.pipe";
 import {DarkContainerComponent} from "../../components/ui/dark-container.component";
+import {PluralPipe} from "../../pipes/plural.pipe";
 
 @Component({
   selector: 'app-room-listing',
@@ -21,6 +22,8 @@ import {DarkContainerComponent} from "../../components/ui/dark-container.compone
     ContainerTitleComponent,
     GamePipe,
     DarkContainerComponent,
+    PluralPipe,
+    NgIf,
   ],
   templateUrl: './room-listing.component.html'
 })
@@ -31,6 +34,17 @@ export class RoomListingComponent {
     client.getRoomListing().subscribe(rooms => {
       this.rooms = rooms.data;
     })
+  }
+
+  playerCount(): number {
+    if(!this.rooms) return -1;
+    let players: number = 0;
+
+    for (let room of this.rooms) {
+      players += room.playerIds.length;
+    }
+
+    return players;
   }
 
   // moronic hack to iterate through the values of an enum
