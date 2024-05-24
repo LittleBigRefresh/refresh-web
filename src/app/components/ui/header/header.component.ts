f222f2842b7ee4dc62144bf612a6d1656cbd8d87
jvyden <jvyden@jvyden.xyz>
2024-05-23 21:50:20 -0400

    Adjust padding for mobile header

--- src/app/components/ui/header/header.component.ts
+++ src/app/components/ui/header/header.component.ts
@@ -13,13 +13,13 @@ import {faCameraAlt, faCertificate, faFireAlt} from "@fortawesome/free-solid-svg
     RouterLink
   ],
   template: `
-    <header class="flex items-center bg-header-background gap-x-2.5 px-5 py-1 leading-none sticky top-0 left-0 w-full">
+    <header class="flex items-center bg-header-background gap-x-2.5 sm:gap-x-1 px-5 py-1 leading-none sticky top-0 left-0 w-full">
       <a routerLink="/" title="Home">
         <img ngSrc="/assets/logo.svg" alt="Refresh Logo" width="48" height="48" priority>
       </a>
 
       <div class="mx-1.5 w-[3px] rounded-full h-11 bg-divider"></div>
-      <nav class="flex gap-x-5">
+      <nav class="flex gap-x-5 sm:gap-x-2">
         <app-navbar-item href="/levels" title="Levels" [icon]=faCertificate></app-navbar-item>
         <app-navbar-item href="/photos" title="Photos" [icon]=faCameraAlt></app-navbar-item>
         <app-navbar-item href="/activity" title="Activity" [icon]=faFireAlt></app-navbar-item>
