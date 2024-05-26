a9c920ec762e97788c66b53bbf75dab80136773f
jvyden <jvyden@jvyden.xyz>
2024-05-25 23:32:03 -0400

    Make textbox required by default

--- src/app/components/ui/form/textbox.component.ts
+++ src/app/components/ui/form/textbox.component.ts
@@ -14,7 +14,7 @@ import {FormGroup, ReactiveFormsModule} from "@angular/forms";
     <label [for]=ctrlName class="text-sm">{{label}}</label>
     <div [formGroup]="form" class="group rounded px-4 py-1.5 bg-teritary focus-within:outline-2 focus-within:outline focus-within:outline-secondary-bright max-w-fit">
       <fa-icon [icon]="icon" class="text-secondary mr-2 group-focus-within:text-secondary-bright"></fa-icon>
-      <input type="text" [id]=ctrlName [formControlName]="ctrlName" [placeholder]="placeholder" class="outline-none bg-teritary placeholder:text-secondary">
+      <input type="text" [id]=ctrlName [formControlName]="ctrlName" [placeholder]="placeholder" class="outline-none bg-teritary placeholder:text-secondary" [required]="required">
     </div>
   `
 })
@@ -25,6 +25,8 @@ export class TextboxComponent {
 
   @Input({required: true}) form: FormGroup = null!;
   @Input({required: true}) ctrlName: string = "";
+
+  @Input() required: boolean = true;
 
   // bg-teritary focus-within:text-secondary-bright text-secondary px-5 py-2 rounded
   // drop-shadow-lg flex focus-within:outline-secondary-bright focus-within:outline-2 focus-within:outline
