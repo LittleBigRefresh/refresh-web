1c82701dc801b4ea601375259e22fc5470023db2
jvyden <jvyden@jvyden.xyz>
2024-05-26 00:04:25 -0400

    Allow specifying text of submit form button

--- src/app/components/ui/form/button-submit-form.component.ts
+++ src/app/components/ui/form/button-submit-form.component.ts
@@ -10,11 +10,12 @@ import {FormGroup} from "@angular/forms";
     ButtonComponent
   ],
   template: `
-    <app-button text="Submit" [icon]="faCheckCircle" color="bg-primary" type="submit" [enabled]="form.status == 'VALID'"></app-button>
+    <app-button [text]="action" [icon]="faCheckCircle" color="bg-primary" type="submit" [enabled]="form.status == 'VALID'"></app-button>
   `,
   styles: ``
 })
 export class ButtonSubmitFormComponent {
   @Input({required: true}) form: FormGroup = null!;
+  @Input() action: string = "Submit";
   protected readonly faCheckCircle = faCheckCircle;
 }
