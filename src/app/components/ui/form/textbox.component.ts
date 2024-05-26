30abe9a816cee967419e7792384f75d1811b2657
jvyden <jvyden@jvyden.xyz>
2024-05-26 00:02:15 -0400

    fix wrapping issues with textbox

--- src/app/components/ui/form/textbox.component.ts
+++ src/app/components/ui/form/textbox.component.ts
@@ -12,7 +12,7 @@ import {FormGroup, ReactiveFormsModule} from "@angular/forms";
   ],
   template: `
     <label [for]=ctrlName class="text-sm">{{label}}</label>
-    <div [formGroup]="form" class="group rounded px-4 py-1.5 bg-teritary focus-within:outline-2 focus-within:outline focus-within:outline-secondary-bright max-w-fit">
+    <div [formGroup]="form" class="group rounded px-4 py-1.5 bg-teritary focus-within:outline-2 focus-within:outline focus-within:outline-secondary-bright max-w-fit text-nowrap">
       <fa-icon [icon]="icon" class="text-secondary mr-2 group-focus-within:text-secondary-bright"></fa-icon>
       <input type="text" [id]=ctrlName [formControlName]="ctrlName" [placeholder]="placeholder" class="outline-none bg-teritary placeholder:text-secondary" [required]="required">
     </div>
