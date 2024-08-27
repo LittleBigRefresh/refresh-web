90d83647184a42558fa687b560f31c81f0901a35
jvyden <jvyden@jvyden.xyz>
2024-08-27 18:49:00 -0400

    Port fade transition from old site over
    I wanted to remove this, but it's actually really good for masking page loads.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,31 @@ import {fas} from "@fortawesome/free-solid-svg-icons";
 import {AuthenticationService} from "./api/authentication.service";
 import {PopupBannerContainerComponent} from "./banners/popup-banner-container.component";
 import {BannerService} from "./banners/banner.service";
+import {animate, group, query, style, transition, trigger} from "@angular/animations";
+
+const fadeLength: string = "100ms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [RouterOutlet, HeaderComponent, PopupBannerContainerComponent],
-  templateUrl: './app.component.html'
+  imports: [RouterOutlet, HeaderComponent, PopupBannerContainerComponent],
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('routeAnimations', [
+      transition('* => *', [
+        group([
+          query(':leave', [
+            style({opacity: 1, 'grid-column': 1, 'grid-row': 1}),
+            animate(fadeLength, style({opacity: 0}))
+          ], {optional: true}),
+          query(':enter', [
+            style({opacity: 0, 'grid-column': 1, 'grid-row': 1}),
+            animate(fadeLength, style({opacity: 1}))
+          ], {optional: true}),
+        ])
+      ])
+    ])
+  ],
 })
 export class AppComponent {
   constructor(private title: TitleService,
