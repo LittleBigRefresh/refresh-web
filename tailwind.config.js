6abdf0aaffeb314f4994a61ebadbb0df0b188d45
jvyden <jvyden@jvyden.xyz>
2024-08-27 18:01:26 -0400

    Add 2xl layer for screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -66,6 +66,7 @@ module.exports = {
       'md': {'min': '768px', 'max': '1023px'},
       'lg': {'min': '1024px', 'max': '1279px'},
       'xl': {'min': '1280px'},
+      '2xl': {'min': '1500px'},
     },
   },
   plugins: [],
