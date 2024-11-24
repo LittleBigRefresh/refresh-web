155284016151c3cb112e06ec3ca2310fc225ebe8
Toaster <brottoastlemon@gmail.com>
2024-11-24 19:29:56 +0100

    Remove and replace some no longer nessesary CSS classes and HTML elements, titleSubtext is next to title again

--- src/app/components/ui/layouts/fancy-header.component.ts
+++ src/app/components/ui/layouts/fancy-header.component.ts
@@ -24,26 +24,25 @@ import { NgTemplateOutlet } from "@angular/common";
       <div class="flex gap-x-2 sm:gap-x-2.5">
         <ng-content select="[avatar]"></ng-content>
         <div class="grow content-evenly">
-          <div class="flex flex-row-reverse justify-between sm:flex-col gap-x-1.5 relative">
-            <div class="content-center">
-              <ng-content select="[buttonArea]"></ng-content>
-            </div>
-            <app-page-title [title]="title" class="text-wrap [word-break:break-word] " ></app-page-title>
+          <div class="flex flex-row place-content-between sm:flex-col gap-x-1.5 relative">
+            <app-page-title [title]="title" class="text-wrap [word-break:break-word]">
+              <ng-container titleSubtext>
+                <span class="align-bottom text-gentle text-base text-wrap font-normal self-center sm:self-auto">
+                  <ng-content select="[titleSubtext]"></ng-content>
+                </span>
+              </ng-container>
+            </app-page-title>
+            <ng-content select="[buttonArea]"></ng-content>
           </div>
           <div class="flex flex-wrap gap-x-4 place-content-between">
             <div class="">
-              <span class="align-bottom text-gentle self-center sm:self-auto text-wrap">
-                <ng-content select="[titleSubtext]"></ng-content>
-              </span>
               <ng-content select="[belowTitle]"></ng-content>
               <ng-content select="[statistics]"></ng-content>
             </div>
           </div>
         </div>
       </div>
-      <div class="content-center">
-        <ng-content select="[buttonAreaMobile]"></ng-content>
-      </div>
+      <ng-content select="[buttonAreaMobile]"></ng-content>
       <div class="mt-2.5">
         <ng-container *ngTemplateOutlet="descriptionTemplate"></ng-container>
       </div>
