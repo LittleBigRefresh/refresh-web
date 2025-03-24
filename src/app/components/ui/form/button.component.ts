8c14a87ee7dc67fab6a15e6acfe8cbc7f7cb68aa
jvyden <jvyden@jvyden.xyz>
2025-03-24 18:15:46 -0400

    Fix extra padding in buttons when no text is specified

--- src/app/components/ui/form/button.component.ts
+++ src/app/components/ui/form/button.component.ts
@@ -14,7 +14,7 @@ import { NgClass } from "@angular/common";
     <button class="rounded px-4 py-1.5 hover:brightness-110 active:brightness-95 transition-[filter] disabled:grayscale" 
       [ngClass]="color" [type]=type [disabled]="!enabled">
       @if (icon) {
-        <fa-icon [icon]="icon" class="mr-1"></fa-icon>
+        <fa-icon [icon]="icon" [ngClass]="text && text.length > 0 ? 'mr-1' : ''"></fa-icon>
       }
       {{ text }}
     </button>
