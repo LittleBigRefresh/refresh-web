51f12d7e4b1dd2109472123d31e21f9179f26019
Toaster2 <brottoastlemon@gmail.com>
2025-11-29 15:06:51 +0100

    Refactor formatting out of DateComponent, unify team pick tooltips

--- src/app/components/items/level-statistics.component.ts
+++ src/app/components/items/level-statistics.component.ts
@@ -2,15 +2,14 @@ import {Component, Input} from '@angular/core';
 import {Level} from "../../api/types/levels/level";
 import {faCircleCheck, faHeart, faPlay, faStar, faThumbsDown, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 import {StatisticComponent} from "../ui/info/statistic.component";
-import { DateComponent } from "../ui/info/date.component";
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { TooltipComponent } from "../ui/text/tooltip.component";
+import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time';
 
 @Component({
     selector: 'app-level-statistics',
     imports: [
     StatisticComponent,
-    DateComponent,
     FaIconComponent,
     TooltipComponent
 ],
@@ -22,21 +21,37 @@ import { TooltipComponent } from "../ui/text/tooltip.component";
       <app-statistic [value]=level.uniquePlays name="Plays" [icon]=faPlay></app-statistic>
       <app-statistic [value]=level.score name="Cool Rating (CR)" [icon]=faStar [truncate]=true></app-statistic>
       @if (level.teamPicked) {
-        <div class="flex flex-row gap-x-1">
-          <app-tooltip text="Team picked">
+        <app-tooltip [text]="'Team picked since ' + this.getFormattedDateTime()">
+          <div class="flex flex-row gap-x-1">
             <fa-icon [icon]="faCircleCheck"></fa-icon>
-          </app-tooltip>
-          
-          @if (level.dateTeamPicked != null) {
-            <app-date [date]="level.dateTeamPicked"></app-date>
-          }
-        </div>
+            <p> {{this.getShortDateTime()}}</p>
+          </div>
+        </app-tooltip>
       }
     </div>
   `
 })
 export class LevelStatisticsComponent {
   @Input({required: true}) level: Level = undefined!;
+
+  ngOnInit() {
+    if (this.level.dateTeamPicked != null) {
+      this.level.dateTeamPicked = new Date(this.level.dateTeamPicked);
+    }
+  }
+
+  protected getFormattedDateTime(): string {
+    if (this.level.dateTeamPicked == null) return "unknown";
+
+    return getFormattedDateTime(this.level.dateTeamPicked);
+  }
+
+  protected getShortDateTime(): string {
+    if (this.level.dateTeamPicked == null) return "";
+
+    return getShortDateTime(this.level.dateTeamPicked);
+  }
+
   protected readonly faThumbsUp = faThumbsUp;
   protected readonly faThumbsDown = faThumbsDown;
   protected readonly faHeart = faHeart;
