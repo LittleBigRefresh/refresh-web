f7ad1814377aef6e97cc58a3dc60af7b039b6349
Toaster2 <brottoastlemon@gmail.com>
2025-11-29 16:00:39 +0100

    Refactor team pick status into an own component

--- src/app/components/items/level-statistics.component.ts
+++ src/app/components/items/level-statistics.component.ts
@@ -1,17 +1,15 @@
 import {Component, Input} from '@angular/core';
 import {Level} from "../../api/types/levels/level";
-import {faCircleCheck, faHeart, faPlay, faStar, faThumbsDown, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
+import {faHeart, faPlay, faStar, faThumbsDown, faThumbsUp} from "@fortawesome/free-solid-svg-icons";
 import {StatisticComponent} from "../ui/info/statistic.component";
-import { FaIconComponent } from "@fortawesome/angular-fontawesome";
-import { TooltipComponent } from "../ui/text/tooltip.component";
 import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time';
+import { LevelTeamPickStatusComponent } from "./level-team-pick-status.component";
 
 @Component({
     selector: 'app-level-statistics',
     imports: [
     StatisticComponent,
-    FaIconComponent,
-    TooltipComponent
+    LevelTeamPickStatusComponent
 ],
     template: `
     <div class="flex flex-wrap gap-x-1.5">
@@ -21,12 +19,7 @@ import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time'
       <app-statistic [value]=level.uniquePlays name="Plays" [icon]=faPlay></app-statistic>
       <app-statistic [value]=level.score name="Cool Rating (CR)" [icon]=faStar [truncate]=true></app-statistic>
       @if (level.teamPicked) {
-        <app-tooltip [text]="'Team picked since ' + this.getFormattedDateTime()">
-          <div class="flex flex-row gap-x-1">
-            <fa-icon [icon]="faCircleCheck"></fa-icon>
-            <p> {{this.getShortDateTime()}}</p>
-          </div>
-        </app-tooltip>
+        <app-level-team-pick-status [level]="level"></app-level-team-pick-status>
       }
     </div>
   `
@@ -57,5 +50,4 @@ export class LevelStatisticsComponent {
   protected readonly faHeart = faHeart;
   protected readonly faStar = faStar;
   protected readonly faPlay = faPlay;
-  protected readonly faCircleCheck = faCircleCheck;
 }
---
+++ src/app/components/items/level-team-pick-status.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input} from '@angular/core';
+import {Level} from "../../api/types/levels/level";
+import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
+import { FaIconComponent } from "@fortawesome/angular-fontawesome";
+import { TooltipComponent } from "../ui/text/tooltip.component";
+import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time';
+
+@Component({
+    selector: 'app-level-team-pick-status',
+    imports: [
+    FaIconComponent,
+    TooltipComponent
+],
+    template: `
+    <app-tooltip [text]="'Team picked since ' + this.getFormattedDateTime()">
+        <div class="flex flex-row gap-x-1">
+        <fa-icon [icon]="faCircleCheck"></fa-icon>
+        <p> {{this.getShortDateTime()}}</p>
+        </div>
+    </app-tooltip>
+  `
+})
+export class LevelTeamPickStatusComponent {
+  @Input({required: true}) level: Level = undefined!;
+
+  ngOnInit() {
+    if (this.level.dateTeamPicked != null) {
+      this.level.dateTeamPicked = new Date(this.level.dateTeamPicked);
+    }
+  }
+
+  protected getFormattedDateTime(): string {
+    if (this.level.dateTeamPicked == null) return "unknown";
+
+    return getFormattedDateTime(this.level.dateTeamPicked);
+  }
+
+  protected getShortDateTime(): string {
+    if (this.level.dateTeamPicked == null) return "";
+
+    return getShortDateTime(this.level.dateTeamPicked);
+  }
+
+  protected readonly faCircleCheck = faCircleCheck;
+}
