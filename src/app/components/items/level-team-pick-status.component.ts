119b0c48cbad9a0bac5f21e3eb2a8661422bfdbe
Toaster2 <brottoastlemon@gmail.com>
2026-02-07 11:27:47 +0100

    Shorten team pick notice for mobile widths

--- src/app/components/items/level-team-pick-status.component.ts
+++ src/app/components/items/level-team-pick-status.component.ts
@@ -4,6 +4,7 @@ import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
 import { FaIconComponent } from "@fortawesome/angular-fontawesome";
 import { TooltipComponent } from "../ui/text/tooltip.component";
 import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time';
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
     selector: 'app-level-team-pick-status',
@@ -16,10 +17,14 @@ import { getFormattedDateTime, getShortDateTime } from '../../helpers/date-time'
       <!-- TODO: Use "primary" once that is a declared color instead of "yellow" to prepare for themes -->
       <div class="flex flex-row gap-x-1 text-yellow">
           <fa-icon [icon]="faCircleCheck"></fa-icon>
+
           @if (!short) {
-            <p>team picked {{this.shortTime}}</p>
+            @if (!isMobile) {
+              <span>team picked </span>
+            }
+
+            <span>{{this.shortTime}}</span>
           }
-          
       </div>
     </app-tooltip>
   `
@@ -30,6 +35,11 @@ export class LevelTeamPickStatusComponent {
 
   protected formattedTime: string = "unknown";
   protected shortTime: string = "";
+  protected isMobile: boolean = false;
+
+  constructor(protected layout: LayoutService) {
+    this.layout.isMobile.subscribe(v => this.isMobile = v);
+  }
 
   ngOnInit() {
     if (this.level.dateTeamPicked != null) {
