d6922026b1aa562822db494c1100083444811bdc
Toaster2 <brottoastlemon@gmail.com>
2026-02-17 13:45:34 +0100

    Emphasize heart stat if user is hearted

--- src/app/components/items/user-statistics.component.ts
+++ src/app/components/items/user-statistics.component.ts
@@ -7,6 +7,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import {StatisticComponent} from "../ui/info/statistic.component";
 import {UserStatistics} from "../../api/types/users/user-statistics";
+import { UserRelations } from '../../api/types/users/user-relations';
 
 @Component({
     selector: 'app-user-statistics',
@@ -15,7 +16,7 @@ import {UserStatistics} from "../../api/types/users/user-statistics";
     ],
     template: `
     <div class="flex gap-x-1.5">
-      <app-statistic [value]=stats.favourites name="Hearts" [icon]=faHeart></app-statistic>
+      <app-statistic [value]=stats.favourites name="Hearts" [icon]=faHeart [emphasize]="(ownRelations?.isHearted ?? false )"></app-statistic>
       <app-statistic [value]=stats.profileComments name="Comments" [icon]=faComment></app-statistic>
       <app-statistic [value]=stats.publishedLevels name="Published Levels" [icon]=faCertificate></app-statistic>
       <app-statistic [value]=stats.photosTaken name="Photos" [icon]=faImages></app-statistic>
@@ -24,6 +25,7 @@ import {UserStatistics} from "../../api/types/users/user-statistics";
 })
 export class UserStatisticsComponent {
   @Input({required: true}) stats: UserStatistics = undefined!;
+  @Input() ownRelations: UserRelations | undefined;
 
   protected readonly faHeart = faHeart;
   protected readonly faComment = faComment;
