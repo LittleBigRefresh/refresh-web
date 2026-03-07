c50f780b672181ee34b41269a8d6fdd2a8cd06ab
Toaster2 <brottoastlemon@gmail.com>
2026-03-07 19:02:15 +0100

    Use box shadow instead of drop shadow for ContainerComponent to fix stacking

--- src/app/components/ui/container.component.ts
+++ src/app/components/ui/container.component.ts
@@ -7,7 +7,7 @@ import { NgClass } from "@angular/common";
         NgClass
     ],
     template: `
-    <div class="bg-container-background rounded drop-shadow-md" [ngClass]="padding ? (tight ? 'px-3.5 py-1.5' : 'px-5 py-2.5') : ''">
+    <div class="bg-container-background rounded shadow-md" [ngClass]="padding ? (tight ? 'px-3.5 py-1.5' : 'px-5 py-2.5') : ''">
       <ng-content></ng-content>
     </div>
   `
