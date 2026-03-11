5e95f6e85545174c46608efe6fff84d25beffa1c
Toaster2 <brottoastlemon@gmail.com>
2026-03-11 23:03:06 +0100

    Button which allows mods to access the user moderation page

--- src/app/components/ui/layouts/fancy-header-user-buttons.component.ts
+++ src/app/components/ui/layouts/fancy-header-user-buttons.component.ts
@@ -3,11 +3,13 @@ import { BannerService } from "../../../banners/banner.service";
 import { ExtendedUser } from "../../../api/types/users/extended-user";
 import { ClientService } from "../../../api/client.service";
 import { ButtonOrNavItemComponent } from "../form/button-or-navitem.component";
-import { faHeart, faHeartCrack } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faHeartCrack, faPencil } from "@fortawesome/free-solid-svg-icons";
 import { FancyHeaderButtonsComponent } from "./fancy-header-buttons.component";
 import { User } from "../../../api/types/users/user";
 import { UserRelations } from "../../../api/types/users/user-relations";
 import { RefreshApiError } from "../../../api/refresh-api-error";
+import { UserRoles } from "../../../api/types/users/user-roles";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-fancy-header-user-buttons',
@@ -16,6 +18,19 @@ import { RefreshApiError } from "../../../api/refresh-api-error";
     FancyHeaderButtonsComponent,
 ],
     template: `
+        <ng-template #moderateButtonTemplate let-templateHasText="hasText" let-templateIsNavItem="isNavItem">
+            <app-button-or-navitem
+                text="Moderate"
+                [icon]="faPencil"
+                color="bg-blue"
+
+                (click)="this.router.navigate(['/u/', targetUser.userId, 'settings'])"
+
+                [hasText]="templateHasText"
+                [isNavItem]="templateIsNavItem"> 
+            </app-button-or-navitem>
+        </ng-template>
+
         <ng-template #heartButtonTemplate let-templateHasText="hasText" let-templateIsNavItem="isNavItem">
             <app-button-or-navitem
                 text="Heart"
@@ -49,19 +64,24 @@ export class FancyHeaderUserButtonsComponent {
     @Input({required: true}) public relations: UserRelations = undefined!;
 
     @ViewChild('heartButtonTemplate') heartButtonTemplateRef!: TemplateRef<any>;
+    @ViewChild('moderateButtonTemplate') moderateButtonTemplateRef!: TemplateRef<any>;
     
     buttonTemplateRefs: TemplateRef<any>[] = [];
     buttonsInitialized: boolean = false;
 
-    constructor(private client: ClientService, private bannerService: BannerService) {}
+    constructor(private client: ClientService, private bannerService: BannerService, protected router: Router) {}
 
     ngAfterViewInit() {
+        // FIXME: these buttons, weirdly, sometimes don't appear on user pages, but refreshing the page makes them appear again.
+        // Moderate Button
+        if (this.ownUser.role === UserRoles.Admin || (this.ownUser.role >= UserRoles.Moderator && this.targetUser.role < UserRoles.Moderator)) {
+            this.buttonTemplateRefs.push(this.moderateButtonTemplateRef);
+        }
+
         // Heart Button
         if (this.ownUser.userId !== this.targetUser.userId) {
             this.buttonTemplateRefs.push(this.heartButtonTemplateRef);
         }
-
-        // TODO: Other buttons, such as an edit button for mods
 
         this.buttonsInitialized = true;
     }
@@ -95,4 +115,5 @@ export class FancyHeaderUserButtonsComponent {
 
     protected readonly faHeart = faHeart; 
     protected readonly faHeartCrack = faHeartCrack;
+    protected readonly faPencil = faPencil;
 }
