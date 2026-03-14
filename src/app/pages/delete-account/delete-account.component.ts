e1e7a2603a83a2e724a6cc5061dfc10441b1d50a
Toaster2 <brottoastlemon@gmail.com>
2026-03-14 19:17:13 +0100

    Allow setting password for account deletion requests, don't falsely show success if deletion failed

--- src/app/pages/delete-account/delete-account.component.ts
+++ src/app/pages/delete-account/delete-account.component.ts
@@ -1,17 +1,29 @@
 import {Component} from '@angular/core';
 import {AuthService} from "../../api/auth.service";
+import { sha512Async } from 'src/app/hash';
+import { faKey } from '@fortawesome/free-solid-svg-icons';
+import { FormHandler } from 'src/app/helpers/FormHandler';
 
 @Component({
     selector: 'app-delete-account',
     templateUrl: './delete-account.component.html'
 })
-export class DeleteAccountComponent {
+export class DeleteAccountComponent extends FormHandler {
+    password: string = "";
+
     constructor(private authService: AuthService) {
+        super();
     }
 
     delete(): void {
-        this.authService.DeleteAccount();
+        const formInputs = this.cleanUpFormInputs(this.password);
+        const [password] = formInputs;
+
+        sha512Async(password).then((hash) => {
+            this.authService.DeleteAccount(hash);
+        });
     }
 
     protected readonly window = window;
+    protected readonly faKey = faKey;
 }
